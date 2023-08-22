7ee7222128490fdffd59789baa74f095cef61349
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-22 20:17:59 +0300

    Задача с четвертями

---
+++ Sem3_Task17/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Первая четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Третья четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Четвёртая четверть");
+}
+else
+{
+    Console.WriteLine("x = 0, y = 0");
+}
