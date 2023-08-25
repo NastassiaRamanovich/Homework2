f52383a30102dcc104a89148f715ebb4ba1210c4
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-25 21:43:33 +0300

    Задача на создание массива из нулей и единиц

---
+++ Sem4_Task30/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int [] array = new int[8];
+// FillArray(array);
+// int FillArray (int [] collection)
+// {
+//     int length = collection.Length;
+//     int i = 0;
+//     while (i < length)
+//     {
+//         array[i] = new Random().Next(0,1);
+//         i++;
+//     }
+// }
+// PrintArray(array);
+
+int[] Array (int number){
+    int[] arr = new int[number];
+    Random rand = new Random();
+    
+    for (int i = 0; i < number; i++){
+        arr[i] = rand.Next(2);
+        Console.Write($"{arr[i]} ");        
+    }
+    
+    return arr;
+}
+
+Array(8);
