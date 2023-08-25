c9f601a9deee0232debbf657617916bc4f98d3cd
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-25 22:04:01 +0300

    Добавила варианты отображения массива

--- Sem4_Task30/Program.cs
+++ Sem4_Task30/Program.cs
@@ -29,3 +29,16 @@ int[] Array (int number){
 }
 
 Array(8);
+
+// варианты отображения массива
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// foreach (int element in array) {
+//     Console.Write(element + " ");
+// }
+// Console.WriteLine();
+// string arrayOutput = String.Join(", ", array);
+// Console.WriteLine("[" + arrayOutput + "]");
