34745cf8615b4ae9a31eb6cb6d6ce2c476ba5183
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-29 21:05:22 +0300

    Нахождение числа в массиве

---
+++ Sem5_Task33/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, -3] -> да
+
+void ArrayRandom(int[] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1, 6);
+    }
+    System.Console.WriteLine(string.Join(" ", array));
+}
+
+bool ArraySearch(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (number == array[i])
+        {
+            return true;
+        }
+
+    }
+    return false;
+}
+
+int[] array = new int [15];
+
+ArrayRandom(array);
+
+bool search = ArraySearch(array, 3);
+
+System.Console.WriteLine(search);
