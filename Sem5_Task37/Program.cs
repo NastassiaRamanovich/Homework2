c2c5d81327c8368964b408918e9323da256b1c70
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-29 22:47:06 +0300

    Задача на нахождение числа в массиве и кол-во раз

---
+++ Sem5_Task37/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Answer
+{
+    public static void Main(string[] args)
+    {
+        // Создаем массив из 123 случайных чисел
+        Random random = new Random();
+        int[] array = new int[123];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 1000);
+        }
+
+        // Находим количество элементов массива, значения которых лежат в отрезке [10,99]
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] >= 10 && array[i] <= 99)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество элементов массива, значения которых лежат в отрезке [10,99]: " + count);
+    }
+}
