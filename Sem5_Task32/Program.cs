7f4543583ee91e9b10b3693ec63b437c41a89ba9
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-29 20:42:54 +0300

    Массив с числами умноженными на -1

---
+++ Sem5_Task32/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+int[]array = new int[4];
+
+fillArray(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+   
+        array[i] = array[i] * (-1);
+    
+}
+Console.WriteLine(String.Join(" ", array));
+void fillArray(int[] array){
+  for (int i = 0; i < array.Length; i++)
+  {
+        array[i] = new Random().Next(-9, -1);       
+  }
+  Console.WriteLine(String.Join(" ", array)); 
+}
